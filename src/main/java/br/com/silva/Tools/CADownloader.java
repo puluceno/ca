f9fae1f3c3794aecd859257984ee1e177e924eb0
pulu <puluceno@gmail.com>
2016-10-26 16:40:54 -0200

    added multiple log configurations and prevented broken files from being created

--- src/main/java/br/com/silva/Tools/CADownloader.java
+++ src/main/java/br/com/silva/Tools/CADownloader.java
@@ -1,13 +1,18 @@
 package br.com.silva.Tools;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.Date;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
+import org.pmw.tinylog.Configurator;
 import org.pmw.tinylog.Logger;
 
 import com.gargoylesoftware.htmlunit.Page;
@@ -30,14 +35,15 @@ public class CADownloader {
 		if (!directory.exists())
 			directory.mkdirs();
 
-		WebClient webClient = initializeClient();
 		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
 		java.util.logging.Logger.getLogger("org.apache.http.client.protocol.ResponseProcessCookies")
 				.setLevel(Level.OFF);
 
 		int numberGenerated = 0;
-		for (int number = 712; number < 100000; number++) {
+
+		for (int number = 3305; number < 100000; number++) {
 			System.out.println("Downloading CA " + number);
+			WebClient webClient = initializeClient();
 			long beginCA = new Date().getTime();
 
 			try {
@@ -50,27 +56,37 @@ public class CADownloader {
 					Page click = anchor.click();
 					WebResponse webResponse = click.getWebResponse();
 					InputStream is = webResponse.getContentAsStream();
-					File file = new File(DIR + number + PDF_EXTENSION);
 
-					OutputStream os = new FileOutputStream(file);
-					byte[] bytes = new byte[8 * 1024];
-					int read;
-					try {
-						while ((read = is.read(bytes)) != 0) {
-							os.write(bytes, 0, read);
+					String result = new BufferedReader(new InputStreamReader(is)).lines()
+							.collect(Collectors.joining("\n"));
+					if (result.contains("informe o seu e-mail no campo abaixo que enviamos")) {
+						logNotFound();
+						Logger.info("{} - CA {} não encontrado na origem.", number, number);
+						logDefault();
+					} else {
+						File file = new File(DIR + number + PDF_EXTENSION);
+
+						OutputStream os = new FileOutputStream(file);
+						byte[] bytes = new byte[1024];
+						int read;
+						try {
+							while ((read = is.read(bytes)) != -1) {
+								os.write(bytes, 0, read);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						} finally {
+							os.close();
+							is.close();
+							webResponse.cleanUp();
+							numberGenerated++;
+							Logger.info("CA {} encontrado e arquivado com o nome {}. Tempo de execução: {}", number,
+									number + PDF_EXTENSION,
+									TimeTools.formatTime((int) ((new Date().getTime() - beginCA) / 1000)));
 						}
-					} catch (Exception e) {
-					} finally {
-						os.close();
-						is.close();
-						webResponse.cleanUp();
-						numberGenerated++;
-						Logger.info("CA {} encontrado e arquivado com o nome {}. Tempo de execução: {}", number,
-								number + PDF_EXTENSION,
-								TimeTools.formatTime((int) ((new Date().getTime() - beginCA) / 1000)));
 					}
 				} else
-					Logger.info("CA {} não encontrado. Tempo de execução: {}", number,
+					Logger.info("CA {} inexistente. Tempo de execução: {}", number,
 							TimeTools.formatTime((int) ((new Date().getTime() - beginCA) / 1000)));
 
 			} catch (Exception e) {
@@ -84,6 +100,22 @@ public class CADownloader {
 		long absoluteEnd = new Date().getTime();
 		Logger.info("A operação total levou {}. {} arquivos foram gerados.",
 				TimeTools.formatTime((int) (absoluteBegin - absoluteEnd) / 1000), numberGenerated);
+	}
+
+	private static void logNotFound() {
+		try {
+			Configurator.fromResource("tinylogNotFound.properties").activate();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void logDefault() {
+		try {
+			Configurator.fromResource("tinylog.properties").activate();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static WebClient initializeClient() {
